0f5dfaffd72858c502fef4f21839b6123f8ba531
fthiaazzahra <117642287+fthiaazzahra@users.noreply.github.com>
2024-06-03 11:10:15 +0700

    Update connect.js

--- db-config/connect.js
+++ db-config/connect.js
@@ -2,13 +2,13 @@ const Sequelize = require('sequelize');
 
 // database connection
 const connection = new Sequelize(
-  'rm_pmk',
+  '',
   'root',
-  'Isnaisna02#*',
+  '',
   {
     host: 'localhost',
-    port: 3306,
-    dialect: 'mysql',
+    port: ,
+    dialect: '',
   },
 );
 
